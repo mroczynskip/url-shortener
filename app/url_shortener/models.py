7474d62d0ee8b9c37385c58199b6a72ff1a0671a
Patryk Mroczyński <patryk.mroczynski@tooploox.com>
2023-03-02 01:07:34 +0100

    Fix formatting

--- app/url_shortener/models.py
+++ app/url_shortener/models.py
@@ -5,8 +5,4 @@ from url_shortener.utils import generate_short_url
 
 class URL(models.Model):
     original = models.URLField(unique=True)
-    shortened = models.CharField(
-        max_length=20,
-        unique=True,
-        default=generate_short_url,
-    )
+    shortened = models.CharField(max_length=20, unique=True, default=generate_short_url)
